a1dcb2fdf2344f8d0bc0426a195f592460af4ea9
Carsten Sauerbrey <carsten.sauerbrey@rohde-schwarz.com>
2025-10-16 14:03:16 +0200

    Some formatting

--- docs/source/Example_CustomTools_MCP.py
+++ docs/source/Example_CustomTools_MCP.py
@@ -1,6 +1,7 @@
 from RsInstrument import RsInstrument
 from RsInstrument.mcp import run, safe_tool
 
+
 @safe_tool  # Decorator to catch and log exceptions and return them as error messages to the agent
 def instrument_fancy_function(resource: str, opc_timeout: int = 5000) -> str:
     """My fancy function for RsInstrument MCP.
@@ -18,11 +19,17 @@ def instrument_fancy_function(resource: str, opc_timeout: int = 5000) -> str:
         # your logic goes here
         return "RsInstrument is awesome."
 
+
 if __name__ == "__main__":
-    run(host="localhost", port=8000, transport="streamable-http", tools=[
-        (
-            "Instrument-Fancy-SCPI",  # Tool name
-            "This is an awesome function",  # Tool description
-            instrument_fancy_function  # Tool function
-        )
-    ])
+    run(
+        host="localhost",
+        port=8000,
+        transport="streamable-http",
+        tools=[
+            (
+                "Instrument-Fancy-SCPI",  # Tool name
+                "This is an awesome function",  # Tool description
+                instrument_fancy_function,  # Tool function
+            )
+        ],
+    )
